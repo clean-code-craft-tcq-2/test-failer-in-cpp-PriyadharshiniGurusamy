55eac522ed5c6e5ff62f00f442ad5b29b9bec930
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2022-01-24 11:53:44 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 #include <assert.h>
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+MajorColorCount = sizeof(majorColor)/sizeof(majorColor[0]);
+MinorColorCount = sizeof(minorColor)/sizeof(minorColor[0]);
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+    for(int i = 0; i < 25;) {
+        std::string colorPair = getColorPair(i);
+        if(!colorPair.empty())
+        {
+            std::cout << i << "|" << colorPair;
+            i++;
+        }
+        else
+        {
+            std::cout << "Error in pair \n"<< i ;
+            break;
         }
     }
-    return i * j;
+    return i;
+}
+
+std::string getColorPair(int pairNumber)
+{
+    std::string colorPair = getMajorColorofPair(pairNumber) + "|" + getMinorColorofPair(pairNumber);
+}
+
+std::string getMajorColorofPair(int pairNumber)
+{
+    int zerobasedIndex = pairNumber - 1;
+    majorColorIndex = zerobasedIndex/MajorColorCount;
+    return std::string majorcolor = majorColor[majorColorIndex];
+}
+
+
+std::string getMinorColorofPair(int pairNumber)
+{
+    int zerobasedIndex = pairNumber - 1;
+    minorColorIndex = zerobasedIndex/MinorColorCount;
+    return std::string minorcolor = minorColor[minorColorIndex];
 }
 
 int main() {
